062cbf54b7a2f87ffde70fbd989e9bd7647cf99b
nking1 <134174354+nking1@users.noreply.github.com>
2023-11-15 22:33:39 -0700

    Update MasterScreen.sh
    Fixed bug where a timeout in MTD.sh could cause the subsequent Screen.sh to crash

--- MasterScreen.sh
+++ MasterScreen.sh
@@ -10,11 +10,15 @@
 #SBATCH --output=basenameScreen.out
 
 module load StdEnv/2020 crest/2.12 
+LinesPerMol=$(echo 2+$(sed -n '1p' AScreen/basename.xyz) | bc -l)
 DIR=$(sed -n "${SLURM_ARRAY_TASK_ID}p" screen.dir) 
 cd $DIR 
-echo Starting task $SLURM_ARRAY_TASK_ID in dir $DIR 
+echo Starting task $SLURM_ARRAY_TASK_ID in dir $DIR
+rm xtb.trj
 export OMP_STACKSIZE=Stacksize
 for n in {1..4}
- do cat ../${DIR::1}$n/xtb.trj >> xtb.trj
+ do Lines=$(wc -l ../${DIR::1}$n/xtb.trj | awk '{print $1}')
+ Rem=$(echo "$Lines % $LinesPerMol" | bc)
+ head -n -$Rem ../${DIR::1}$n/xtb.trj >> xtb.trj
  done
 crest -screen xtb.trj -T 28 -g Solvent --chrg CHARGE > basename.out
